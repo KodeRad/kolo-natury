be8e047c7194d1edff70754ccffc712cb1e372db
KodeRad <konrad.krasocki@gmail.com>
2025-04-26 13:39:00 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import koloNaturyLogo from './assets/kolo-natury.PNG'
+import './App.css'
+
+function App() {
+
+    return (
+        <div>
+            <h1>👷‍ ️Work in progress!</h1>
+            <h1>Nowy Młyn - Koło Natury</h1>
+            <a href="https://react.dev" target="_blank">
+                <img src={koloNaturyLogo} alt="React logo"/>
+            </a>
+        </div>
+    )
+}
+
+export default App
