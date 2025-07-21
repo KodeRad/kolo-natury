4301b3598060f0d02c77a5dd8611b9ae1a65883d
KodeRad <konrad.krasocki@gmail.com>
2025-07-21 15:16:01 +0200

    add github workflow

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: isProd ? true : undefined, // Disable default image optimization
   },
-  assetPrefix: isProd ? '/kolo-natury/' : '',
+  // assetPrefix: isProd ? '/kolo-natury/' : '',
   basePath: isProd ? '/kolo-natury' : '',
   output: isProd ? 'export' : undefined
 };
